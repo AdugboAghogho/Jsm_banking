ae47fe55a7b7bd1f2024c62366f785effeee4c04
adugbo aghogho <adugboaghogho@gmail.com>
2024-12-04 23:40:11 +0100

    ww

--- jsm_banking/components/TotalBalanceBox.tsx
+++ jsm_banking/components/TotalBalanceBox.tsx
@@ -14,6 +14,7 @@ const TotalBalanceBox = ({
         <h2 className="header-2">
           Bank Accounts: {totalBanks}
         </h2>
+        
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">
             Total Current Balance
