01a5b2a9364f8af5874650d564e39800ccc6232e
adugbo aghogho <adugboaghogho@gmail.com>
2024-12-05 02:48:31 +0100

    omo

--- jsm_banking/app/(root)/layout.tsx
+++ jsm_banking/app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-// import MobileNav from "@/components/MobileNav";
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 // import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
@@ -22,7 +22,7 @@ export default async function RootLayout({
         <div className="root-layout">
           <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
-            {/* <MobileNav user={loggedIn} /> */}
+            <MobileNav user={loggedIn} />
           </div>
         </div>
         {children}
