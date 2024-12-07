8f1af9d77d491623dc4f4f8856d44801f5fd1d7c
adugbo aghogho <adugboaghogho@gmail.com>
2024-12-07 20:24:54 +0100

    thrr

--- jsm_banking/components/AuthForm.tsx
+++ jsm_banking/components/AuthForm.tsx
@@ -45,41 +45,41 @@ const AuthForm = ({ type }: { type: string }) => {
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true);
 
-    //   try {
-    //     // Sign up with Appwrite & create plaid token
+      try {
+        // Sign up with Appwrite & create plaid token
         
-    //     // if(type === 'sign-up') {
-    //     //   const userData = {
-    //     //     firstName: data.firstName!,
-    //     //     lastName: data.lastName!,
-    //     //     address1: data.address1!,
-    //     //     city: data.city!,
-    //     //     state: data.state!,
-    //     //     postalCode: data.postalCode!,
-    //     //     dateOfBirth: data.dateOfBirth!,
-    //     //     ssn: data.ssn!,
-    //     //     email: data.email,
-    //     //     password: data.password
-    //     //   }
+        if(type === 'sign-up') {
+          const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            ssn: data.ssn!,
+            email: data.email, 
+            password: data.password
+          }
 
-    //     //   const newUser = await signUp(userData);
+          const newUser = await signUp(userData);
 
-    //     //   setUser(newUser);
-    //     // }
+          setUser(newUser);
+        }
 
-    //     if(type === 'sign-in') {
-    //       const response = await signIn({
-    //         email: data.email,
-    //         password: data.password,
-    //       })
+        if(type === 'sign-in') {
+          const response = await signIn({
+            email: data.email,
+            password: data.password,
+          }) 
 
-    //       if(response) router.push('/')
-    //     }
-    //   } catch (error) {
-    //     console.log(error);
-    //   } finally {
-    //     setIsLoading(false);
-    //   }
+          if(response) router.push('/')
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
   return (
