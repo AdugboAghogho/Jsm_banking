e084d8e6ffdc2ac5d5fec1848a4bbe41d6d28400
adugbo aghogho <adugboaghogho@gmail.com>
2024-12-14 11:19:18 +0100

    otto

---
+++ jsm_banking/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3bfc9a9723b4daba8e901bf50b0a5fc0@o4506961017503744.ingest.us.sentry.io/4508462492221440",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
